50e98a0c6fcbeac42b29f61f004049ae63e6a6f1
fxberry <antgeneral@gmail.com>
2014-07-20 11:18:40 +0200

    added

---
+++ Scripts/main.js
@@ -0,0 +1,44 @@
+﻿
+
+$(document).ready(function() {
+
+    console.log("document loaded");
+
+    var hideAll = function() {
+        $('#list').hide();
+        $('#detail').hide();
+    };
+
+    hideAll();
+
+    var startupUrl = '#/list';
+
+    var sammy = new Sammy.Application(function () {
+        
+        this.get('#/list', function (context) {
+            hideAll();
+            $('#list').show();
+
+
+            context.log('list');
+        });
+
+
+        this.get('#/detail', function (context) {
+            hideAll();
+            $('#detail').show();
+            
+            // load from 
+
+            context.log('detail');
+        });
+
+        this.get('', function() {
+            this.app.runRoute('get', startupUrl);
+        });
+
+    });
+
+    sammy.run();
+
+});
